8adc9377f89d64ce699bd412755337cef0f3169d
Süleyman Sevimli <suleyman.sevimli1@gmail.com>
2018-11-24 14:37:29 +0300

    edit

--- HttpClass.php
+++ HttpClass.php
@@ -3,7 +3,8 @@
 	class HttpInfo {
 		// 
 		public function __construct (){
-			// return print_r($_SERVER);
+			return print_r($_SERVER);
+
 		}
 
 		// $_SERVER['PARAMETRE'] kullanımını kısaltmak için kullanılan fonksiyon
@@ -11,8 +12,6 @@
 			return $_SERVER[$var];
 		}
 
-
-		/* Spesifik Alan */ 
 
 		// atanan UNIQUE_ID değerinin değişip değişmediğinin kontrolü
 		public function is_changed_unique_id(){
@@ -24,29 +23,58 @@
 				echo "Your Are A Bot ? Please standart login my page !";
 			} 
 			// örnek bir txt dosya oluşturuyoruz ve içerisine id'leri yazdırıyoruz
-			$file = touch('id.txt');
-	      	$file_o = fopen('id.txt','a+');
-	     	$file_w = fwrite($file_o,$id);
-	      	$file_c = fclose($file_o);
-	      	
+			$this-> fileWrite('id.txt','a+',$id);
+
 	      	// dosyamızı dizi haline getiriyoruz.
 	      	$dizi = explode("\n", file_get_contents('id.txt'));
 	      	
-	      	# print_r($dizi);
+	      	#print_r($dizi);
 	      	
 	      	if (isset($dizi[2]) && $dizi[1] != $dizi[2] ){
 	      		unlink('id.txt');
+	      		
 	      		throw new Exception('Geçersiz UNIQUE ID !');
-	      		echo $e->geMessage();
-	      		unlink('id.txt');
+	      			echo $e->geMessage();
 	      		exit;
-	      	} 
-	      	
-	      	
-	      	
-			
+	      	}
 		}
 
-		/* # Spesifik alan Sonu */
+		// Yönlendirme nereden geliyor tespiti ve değiştirilmesi için;
+		public function referer($change = null){
+			# referans bilgisini aldık.
+			$ref = $this->serverParameter("HTTP_REFERER").PHP_EOL;
+			
+			# eğer referans bilgilerini değiştirmek istiyorsak; 
+			if (isset($change)) {
+				$ref = $change;
+			}
+			
+			#eğer ki bu verileri kullanacaksak bir dosyaya kaydedelim;
+			$this->fileWrite("referer.txt","a+",$ref);
+			# unlink('referer.txt');
+			return  $ref;
+		}
+		
+		public function accept(array $change=null){
+			$http_access = $_SERVER['HTTP_ACCEPT'];
+			$arr = explode(",", $http_access);
+
+			if (isset($change)) {
+				$values = func_get_args();
+				$count = count($values); 
+
+				$arr = $change;
+			}
+
+			print_r($arr);
+		}
+
+		
+		private function fileWrite($fileName,$parameter,$content){
+			$file = touch($fileName);
+	      	$file_o = fopen($fileName,$parameter);
+	     	$file_w = fwrite($file_o,$content);
+	      	$file_c = fclose($file_o);
+		}
 
 	}
--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 	require 'HttpClass.php';
 
 	$server = new HttpInfo();
+
+	// klasik kullanımın dışında bir kullanım için ; 
 	$return = $server->serverParameter("QUERY_STRING");
-	$unique = $server->is_changed_unique_id();
-	print_r($unique);
+
+	// sayfa yenilenmelerini takip etmek için kullanılabilecek olan fonksiyon;
+	# $unique = $server->is_changed_unique_id();
+
+	$referer = $server->referer();
+	// çıktımızı alalım.
+
+	$accept = $server->accept("text/plain");
+	print_r($accept);
+			
